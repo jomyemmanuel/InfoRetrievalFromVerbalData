f31b4957271c4edd0762a79b63cd578a8d8b160f
Amal Majeed <amalmajeed7@gmail.com>
2016-12-07 18:32:48 +0530

    Splitting the diarized segments Done 1

--- diarize-jruby/split.py
+++ diarize-jruby/split.py
@@ -1,28 +1,55 @@
+#### -----------ALL IMPORTS AND DEPENDENCIES-------------- 
 import warnings
 
 warnings.filterwarnings("ignore")
 
 from pydub import AudioSegment
 from filter_parameters import filterout
+import os
 
-#Inputting the audio file
+####---------Relative Path setting
+
+base = os.path.dirname(__file__)
+
+
+username = raw_input("User name:")
+
+####---------Inputting the audio file-------------
 
 inp = raw_input("Audio Title:")
 voice = None
 
 if(inp[-3:] == "wav"):
-	voice = AudioSegment.from_wav(inp)
+	voice = AudioSegment.from_wav(base + 'UserFiles/' + username + '/' + inp)
 elif(inp[-3:] == "mp3"):
-	voice = AudioSegment.from_mp3(inp)
+	voice = AudioSegment.from_mp3(base + 'UserFiles/' + username + '/' + inp)
 
 d = filterout()
 
+#####------- A UNIQUE LIST OF SPEAKER IDs---------
+listofspeakers = list()
+
 for elem in sorted(d):
-	print elem,d[elem][0],d[elem][1]
-# start=input("Start Time:")
-# start=round(start*1000,0)
-# duration=input("Duration of Speech:")
-# duration=round(duration*1000,0)
-# end=start+duration
-# part=voice[start:end]
-# part.export("out.wav", format="wav")
+	listofspeakers.append(d[elem][0].rstrip('\n').rstrip('"').lstrip('"'))
+	#print elem,d[elem][0],d[elem][1]
+
+listofspeakers = list(set(listofspeakers))
+
+####--------- Folder division for user-----------
+
+indexcount=dict()
+
+for x in listofspeakers:
+	os.system('mkdir ' + base + 'UserFiles/' + username + '/' + x)
+	indexcount[x] = 1
+
+
+####----------Audio Splitting and Populating the folders of the user----------
+
+for elem in sorted(d):
+	start = round(elem*1000,0)
+ 	duration = round(float(d[elem][1])*1000,0)
+ 	end = start + duration
+ 	part = voice[start:end]
+ 	part.export(base + 'UserFiles/' + username + '/' + d[elem][0].rstrip('\n').rstrip('"').lstrip('"') + '/' + str(indexcount[d[elem][0].rstrip('\n').rstrip('"').lstrip('"')]) + '.wav', format="wav")
+ 	indexcount[d[elem][0].rstrip('\n').rstrip('"').lstrip('"')] = indexcount[d[elem][0].rstrip('\n').rstrip('"').lstrip('"')] + 1
