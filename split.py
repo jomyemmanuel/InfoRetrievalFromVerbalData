799060ca38c90f69113a17eede635e4174cfea0e
Amal Majeed <amalmajeed7@gmail.com>
2016-12-07 14:19:18 +0530

    Basic Splitting started

--- split.py
+++ split.py
@@ -13,3 +13,11 @@ if(inp[-3:]=="wav"):
 	voice = AudioSegment.from_wav(inp)
 elif(inp[-3:]=="mp3"):
 	voice = AudioSegment.from_mp3(inp)
+
+start=input("Start Time:")
+start=round(start*1000,0)
+duration=input("Duration of Speech:")
+duration=round(duration*1000,0)
+end=start+duration
+part=voice[start:end]
+part.export("out.wav", format="wav")
