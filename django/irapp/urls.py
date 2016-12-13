a746d58ec4e114e87c363d26a277825d914fd26e
Varun-g <varung6395@gmail.com>
2016-12-13 14:50:47 +0530

    nothing

--- django/irapp/urls.py
+++ django/irapp/urls.py
@@ -2,17 +2,14 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.views.static import serve
-import views
-
+from .views import *
 urlpatterns = [
-
-	# url(r'^register/$', views.Registration, name = 'Registration'),
-	# url(r'^login/$', views.login, name = 'login'),
+url(r'^upload/$', views.upload, name = 'upload'),
 ]
 
 if settings.DEBUG:
-		urlpatterns += [
-			url(r'^media/(?P<path>.*)$', serve, {
-				'document_root': settings.MEDIA_ROOT,
-				}),
-		]
+	urlpatterns += [
+	url(r'^media/(?P<path>.*)$', serve, {
+		'document_root': settings.MEDIA_ROOT,
+		}),
+	]
--- django/irapp/views.py
+++ django/irapp/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse,HttpResponseRedirect
 
 # Create your views here.
-from .form import UploadFileForm
+from .forms import UploadFileForm
 
 def valid_audio(file):
 	print file
 
-def upload(request):
-	if request.method == 'POST':
-		form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-        	if valid_audio(request.FILES['file'])
-
-    return render(request,'upload.html',{'form': form})
+	def upload(request):
+		if request.method == 'POST':
+			form = UploadFileForm(request.POST, request.FILES)
+			if form.is_valid():
+				if valid_audio(request.FILES['file']):
+					pass
+					return render(request,'upload.html',{'form': form})
