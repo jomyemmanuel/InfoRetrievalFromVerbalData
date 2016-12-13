20aa2c320e0c3a448d13772801fa938cee2e0328
Varun-g <varung6395@gmail.com>
2016-12-13 15:11:11 +0530

    no

--- django/irapp/forms.py
+++ django/irapp/forms.py
@@ -1,10 +1,14 @@
 from django import forms
 from django.forms import ModelForm
-from .models import user
+# from .models import user
 
-class UserForm(ModelForm):
-	password = forms.CharField(widget=forms.PasswordInput)
+class UploadFileForm(forms.Form):
+    title = forms.CharField(max_length=50)
+    file = forms.FileField()
+    
+# class UserForm(ModelForm):
+# 	password = forms.CharField(widget=forms.PasswordInput)
 
-	class Meta:
-		model = User
-		fields = '__all__'
+# 	class Meta:
+# 		model = User
+# 		fields = '__all__'
--- django/irapp/urls.py
+++ django/irapp/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.views.static import serve
-from .views import *
+from .views import upload
 urlpatterns = [
 url(r'^upload/$', views.upload, name = 'upload'),
 ]
