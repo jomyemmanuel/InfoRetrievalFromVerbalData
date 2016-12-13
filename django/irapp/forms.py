a3d3822d97091c30e16c5f237fbb6253125b390a
Varun-g <varung6395@gmail.com>
2016-12-13 14:57:07 +0530

    nothing

--- django/irapp/forms.py
+++ django/irapp/forms.py
@@ -1,10 +1,14 @@
 from django import forms
 from django.forms import ModelForm
-from .models import User
+# from .models import User
 
-class UserForm(ModelForm):
-	password = forms.CharField(widget=forms.PasswordInput)
+# class UserForm(ModelForm):
+# 	password = forms.CharField(widget=forms.PasswordInput)
+# from django import forms
 
-	class Meta:
-		model = User
-		fields = '__all__'
+class UploadFileForm(forms.Form):
+    title = forms.CharField(max_length=50)
+    file = forms.FileField()
+	# class Meta:
+	# 	model = User
+	# 	fields = '__all__'
